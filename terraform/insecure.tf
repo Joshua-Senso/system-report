ab261b6996e390d237e37b4536251c6b18bd8973
Joshua-Senso <ted.joshua@senso.ph>
2026-02-06 09:07:00 +0800

    test: introduce insecure sg rule for tfsec

---
+++ terraform/insecure.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "bad_ssh" {
+  name        = "bad-ssh"
+  description = "INTENTIONAL: insecure for tfsec learning"
+
+  ingress {
+    description = "SSH open to the world (bad)"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
